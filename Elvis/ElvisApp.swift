dd5570439bf759fe141839e6942e61e09a6baaa3
wlski <105810032+wlski@users.noreply.github.com>
2025-09-02 13:50:43 -0400

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by William Counselman on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
