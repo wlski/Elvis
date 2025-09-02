028826e1c8d47e54e129d6461901cde51d75dbf1
wlski <105810032+wlski@users.noreply.github.com>
2025-09-02 14:07:52 -0400

    elfis

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    @State private var message = ""
+    @State private var imageName = ""
+    
+        var body: some View {
+            VStack {
+                Text( "What's So Funny 'Bout")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundStyle(.purple)
+                Spacer ()
+                Image (systemName: imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                Text (message)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundStyle(.purple)
+                Spacer ()
+                HStack {
+                    Button ("Peace") {
+                        message = "Peace"
+                        imageName = "peacesign"
+                    }
+                    Button ("Love") {
+                        message = "Love"
+                        imageName = "heart"
+                    }
+                    Button ("Understanding") {
+                        message = "Understanding"
+                        imageName = "lightbulb"
+                    }
+                }
+                .tint(.purple)
+                .buttonStyle(.borderedProminent)
+            }
+            .padding()
     }
 }
 
